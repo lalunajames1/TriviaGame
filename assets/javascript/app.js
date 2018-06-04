32e7c999f2bd217bb0c4191226d0b7966a4b66a9
Luis Luna <luisluna@GHC-SEO-0032.globalhc.io>
2018-06-03 19:47:56 -0500

    fixed a few things

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,21 @@
 $(document).ready(function() { 
     
 var numQuestion = 0;
-var userPick;
+var userPick = " ";
+var correctAnswer = " ";
+var congrats = "Congratulations! The answer is ";
+var wrong = "The right answer is ";
 
 
+// Array of questions
 var questions = [
 "He was a pioneer of rock & roll and one of his songs was feature on the movie Back to the Future",
 "Which American folk singer from the 60\’s recently won a Nobel prize?",
 "Who is the god father of Punk rock?",
-"Who created the song Rock the Casbah?",
 "Name a popular band from the 1960s British invasion?",
 ]
 
+//Object with asnwers
 var answers = [
     { 
         ans1: "Little Richards", 
@@ -24,60 +28,91 @@ var answers = [
         ans1: "Bob Dylan", 
         ans2: "Neil Young", 
         ans3: "Johnny Cash", 
-        ans4: "Townes Van Zandt" 
+        ans4: "Townes Van Zandt", 
+        correctAns: "Bob Dylan" 
     },
     {
         ans1: "Joey Ramone",
         ans2: "Iggy Pop",
         ans3: "Joe Strummer",
-        ans4: "Chuck Berry"
+        ans4: "Chuck Berry",
+        correctAns: "Iggy Pop" 
+    },
+    {
+    ans1: "The Doors",
+    ans2: "The Beach Boys",
+    ans3: "The Bealtes",
+    ans4: "Credence Clearwater Revival",
+    correctAns: "The Bealtes"
     }
 ]
 
+//This starts the game 
+
+//This clones the buttons for the ansewers and removes them
 var clone = $("#gone-at-first").clone();
 $("#gone-at-first").remove();
-  
+//This function removes the first button you see and adds back the answer buttons and the question
+
+
 $("#firstButton").on("click", function(){
-    $("#firstButton").remove();
-    $("#main-content").append(clone);
-    grabQuestion();
-    correctFalse();
-    
-  
+    $(this).remove();
+    addButtons() //Adds answer buttons
+    grabQuestion(); //  **** Why do I have to do this? ****
 });
 
+//Adds answer buttons
+function addButtons() {
+$("#main-content").append(clone);
+correctFalse()
+}
+
+//This assigns a answer as a value to the button and an answer as text to the button and increases the numQuestion by 1
 function sendAnswers(){
-    for (let i = 1; i < 5; i++ ){
-       $(".answer" + i).attr("data-value", answers[numQuestion]["ans" + i])
-        $(".answer" + i).text(answers[numQuestion]["ans" + i])
+    for (let i = 1; i <= 4; i++ ){
+       $("#answer" + i).attr("data-value", answers[numQuestion]["ans" + i]);
+       $("#answer" + i).text(answers[numQuestion]["ans" + i]);
 // answers[0].ans1 This is the same as the code above
 // answers[0].ans3
 // answers[0].ans4
 console.log("working") // This helps see if this function is working
     }
+    correctAnswer = answers[numQuestion]["correctAns"];
     numQuestion++
+    console.log(numQuestion)
 }
 
-
+//This adds a question to the html document and executes the sendAnswers function
 function grabQuestion(){
-   $("#question").text(questions[0]);
+   $("#question").text(questions[numQuestion]);
    sendAnswers();
-   
 }
 
-let correctAnswer = answers[numQuestion]["correctAns"];
-
-function correctFalse(){
+//This listens to the click event and grabs the balue form the button to compare it to the correct asnwer
+function correctFalse(){ //Why do I have to create this wrapper function and call it?
   $(".btn").on("click", function(){
-     userPick = $(".btn").attr("data-value");
+     userPick = $(this).attr("data-value");
        console.log(userPick);
        console.log(correctAnswer);
        if (userPick === correctAnswer){
-           console.log("correct");
-       } 
+       correctAnswerMsg();
+       } else {
+           wrongAnswerMsg();
+       }
      })
-
    }
 
+  function correctAnswerMsg(){
+         $("#question").text(congrats + " " + correctAnswer);
+         $("#gone-at-first").remove();
+         setTimeout(function(){ addButtons(); grabQuestion();  }, 3000);
+    
+  }
+ 
+  function wrongAnswerMsg(){
+    $("#question").text(wrong +  " " + correctAnswer);
+    $("#gone-at-first").remove();
+    setTimeout(function(){ addButtons(); grabQuestion();  }, 3000);
+  }
 
 });
